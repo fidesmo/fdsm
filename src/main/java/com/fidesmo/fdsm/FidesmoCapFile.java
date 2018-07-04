323f915e9f424fbc0c268404a673c9410249ae83
Martin Paljak <martin@martinpaljak.net>
2018-07-04 12:35:37 +0300

    Reduce code duplication and add comments

--- src/main/java/com/fidesmo/fdsm/FidesmoCapFile.java
+++ src/main/java/com/fidesmo/fdsm/FidesmoCapFile.java
@@ -8,27 +8,29 @@ import pro.javacard.CAPPackage;
 import java.io.IOException;
 import java.io.InputStream;
 
-// Handy wrapper to detect platform types
+// Handy wrapper to detect platform types. This is not very exact, it assumes that
+// the applet is linked against exact platform exp files (R1 is loadable to R2)
 public class FidesmoCapFile extends CAPFile {
     public FidesmoCapFile(InputStream in) throws IOException {
         super(in);
     }
 
-    public boolean isJCOP242R2() {
+    private boolean isJCOPX(String version) {
         AID jcop = new AID(HexUtils.hex2bin("D276000085494A434F5058"));
         for (CAPPackage p : getImports()) {
-            if (p.getAid().equals(jcop) && p.getVersionString().equals("8.0"))
+            if (p.getAid().equals(jcop) && p.getVersionString().equals(version))
                 return true;
         }
         return false;
     }
 
+    public boolean isJCOP242R2() {
+        // JC 3.0.1, GP 2.2.1, JCOPX 8.0
+        return isJCOPX("8.0");
+    }
+
     public boolean isJCOP242R1() {
-        AID jcop = new AID(HexUtils.hex2bin("D276000085494A434F5058"));
-        for (CAPPackage p : getImports()) {
-            if (p.getAid().equals(jcop) && p.getVersionString().equals("7.0"))
-                return true;
-        }
-        return false;
+        // JC 3.0.1, GP 2.1.1, JCOPX 7.0
+        return isJCOPX("7.0");
     }
 }
