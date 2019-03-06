18f0a2e51a7f06658b57d958af4764e761bfe5a5
Martin Paljak <martin@martinpaljak.net>
2019-03-06 11:00:16 +0200

    Preparations for next version
    - Make use of CallbackHandlers and Logger from ServiceDeliverySession
    - Allow to specify the output stream where FidesmoApiClient trace is written to
    - Introduce timeouts and retries to service delivery (default is 10 minutes)
    - Make FormHandler also implement CallbackHandler
    - Introduce a special --qa argument and ability to fake a Fidesmo card

--- src/main/java/com/fidesmo/fdsm/FormHandler.java
+++ src/main/java/com/fidesmo/fdsm/FormHandler.java
@@ -1,8 +1,9 @@
 package com.fidesmo.fdsm;
 
+import javax.security.auth.callback.CallbackHandler;
 import java.util.List;
 import java.util.Map;
 
-public interface FormHandler {
+public interface FormHandler extends CallbackHandler {
     Map<String, Field> processForm(List<Field> form);
 }
