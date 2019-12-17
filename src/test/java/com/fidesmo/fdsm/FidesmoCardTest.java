41bb574533a1e2720cd0273268dfdcef8bd52b0f
Sergey Khruschak <sergey.khruschak@fidesmo.com>
2019-12-17 18:14:00 +0200

    more card reading tests

--- src/test/java/com/fidesmo/fdsm/FidesmoCardTest.java
+++ src/test/java/com/fidesmo/fdsm/FidesmoCardTest.java
@@ -36,6 +36,20 @@ public class FidesmoCardTest {
         FidesmoCard card = FidesmoCard.getInstance(channel);
         assertEquals("3d5f8004132eda", Hex.encodeHexString(card.getCIN()));
         assertEquals("0000c7", Hex.encodeHexString(card.getBatchId()));
+    }
 
+    @Test
+    public void testCorrectParsingCardInfo() throws CardException {
+        TestChannel channel = TestChannel.fromStrings(
+                "6F108408A000000151000000A5049F6501FF9000",
+                "042457DA3D5F809000",
+                "9F7F2A47906B644700E4D80300816501062899064800000000000000005758594E4E4E4E4E00000000000000009000",
+                "3D5F8004132EDA9000",
+                "420300008C4306023967FE8B419000"
+        );
+
+        FidesmoCard card = FidesmoCard.getInstance(channel);
+        assertEquals("3d5f8004132eda", Hex.encodeHexString(card.getCIN()));
+        assertEquals("00008c", Hex.encodeHexString(card.getBatchId()));
     }
 }
